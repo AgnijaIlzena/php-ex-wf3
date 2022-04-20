1d5764c42b1cb8473f1724beb27e77360757c968
Agnija Ilzena <ilzena.agnija@gmail.com>
2022-04-20 13:55:19 +0200

    upd

--- php/02ex.php
+++ php/02ex.php
@@ -8,7 +8,7 @@ $capitals = [
     'Hongrie'=> 'Budapest',
 ];
 
-// Ex1#2  Trier les valeurs du tableau par ordre alphabétique
+// Ex1#2  Trier les valeurs du tableau par ordre alphabétique (mieux asort())
 ksort($capitals);
 foreach ($capitals as $country => $capital) {
          echo "$country: $capital , ";
@@ -33,6 +33,45 @@ echo '</table>';
 echo '<hr>';
 echo '<br>';
 
+?>
+<!-- ------------------------------------OR -->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><Table></Table></title>
+</head>
+<body>
+    <h1>Pays</h1>
+    <table>
+        <thead>
+            <tr>
+                <th>Pays</th>
+                <th>Capitale</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+            foreach ($capitals as $country => $capital):
+            ?>
+            <tr>
+                <td><?php echo $country;?></td>
+                <td><?php echo $capital;?></td>
+            </tr>
+            <?php
+            endforeach;
+            ?>
+        </tbody>
+    </table>
+</body>
+</html>
+
+<?php
+echo '<hr>';
+echo '<br>';
 // Ex1#4  De nouvelles données viennent d’arriver et Martine en est à son troisième Xanax, il faut l’aider !
 // Insérer les nouvelles données reçues dans votre tableau actuel : 
 
@@ -41,15 +80,36 @@ $array2 = [
     'Allemagne' => 'Berlin'
 ];
 
-$result = array_merge( $capitals, $array2 );
+//$result = array_merge( $capitals, $array2 ); // nav jaataisa jauns variable lai salaadeetu dazhaadu array datus, var mergot virsuu ieprieksheejam.
+$capitals = array_merge($capitals, $array2);
 
+// -----------------------------OR v2
+
+$capitals['Angleterre'] = 'Londres';
+$capitals['Allemagne'] = 'Berlin';
+
+// -----------------------------OR v3
+
+$capitals += $array2;
+
+// -----------------------------OR v4
+
+//spread operator
+$capitals = [...$capitals, ...$array2];
+
+// -----------------------------OR v5
+
+$array2 = [
+    ...$capitals,
+    'Angleterre' => 'Londres',
+    'Allemagne' => 'Berlin'
+];
 
 // Ex1#5 Afficher le tableau grâce à une fonction réservé au débogage. 
 
 echo '<pre>';
-var_dump($result);
+var_dump($capitals);  // or print_r();
 echo '</pre>';
-
 
 echo '<hr>';
 echo '<br>';
@@ -61,6 +121,27 @@ $colors = [
     ['cName' => 'Cyan', 'hex' => '#00FFFF', 'rgb' => 'rgb(0, 255, 255)' ],
     ['cName' => 'Rose', 'hex' => '#FFC0CB', 'rgb' => 'rgb(255, 192, 203)' ]
 ];
+
+//-------------------OR
+
+$couleurs = [
+    'Rouge' => [
+        'hex' => '#FF0000',
+        'rgb' => 'rgb(255, 0, 0)'
+    ],
+    'Orange' => [
+        'hex' => '#FF4500',
+        'rgb' => 'rgb(255, 69, 0)'
+    ],
+    'Cyan' => [
+        'hex' => '#00FFFF',
+        'rgb' => 'rgb(0, 255, 255)'
+    ],
+    'Rose' => [
+        'hex' => '#FFC0CB',
+        'rgb' => 'rgb(255, 192, 203)'
+    ]
+    ];
 
 // Ex2#2   Dans une liste, vous devez afficher le nom de la couleur et son code RGB à côté. 
 
@@ -74,6 +155,48 @@ echo '</ul>';
 
 echo '<hr>';
 echo '<br>';
+
+//-----------------------------------OR
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<ol>
+    <?php
+    foreach($couleurs as $couleur => $code):
+    ?>
+
+    <li><?php echo $couleur; ?> : <?php echo $code['rgb']?></li>
+
+    <?php 
+    endforeach;
+    ?>
+</ol>
+<ol>
+    <?php
+    foreach($couleurs as $couleur => $code):
+    ?>
+
+    <li><?php echo $couleur; ?> : <?php echo $code['hex']?></li>
+
+    <?php 
+    endforeach;
+    ?>
+</ol>
+    
+</body>
+</html>
+
+<?php
 
 // Ex2#3  Dans une seconde liste, vous devez afficher le nom de la couleur et son code hexadécimal à côté
 
---
+++ php/03ex.php
@@ -0,0 +1,99 @@
+<?php
+
+/** EX.1 #1
+ * Créer un tableau numéroté « $temperatures » content les éléments suivants :
+ *  12, 34, -10, 32, 27, 15, 40, -1, 23, -4, 22
+ **/
+
+$temperatures = [12, 34, -10, 32, 27, 15, 40, -1, 23, -4, 22];
+
+// Ex1 #2   •Afficher la moyenne des températures dans une phrase;
+$avTemperatures = round(array_sum($temperatures) / count($temperatures));
+echo "La moyenne temperature est $avTemperatures";
+echo '<br>';
+
+// Ex1 #3  • Afficher le nombre d’éléments contenus dans le tableau.
+echo $elements = count($temperatures);
+echo '<br>';
+
+// Ex1 #3 Additionner l’ensemble des températures et afficher le résultat.
+echo $sumElements = array_sum($temperatures);
+echo '<br>';
+
+// Ex1 #4 Trier les valeurs du tableau en ordre croissant.
+sort($temperatures);
+foreach ($temperatures as $key) {
+    echo $key . ' ; ';
+}
+echo '<br>';
+
+// Ex2 #1
+$myArray = [
+    'Particuliers' => [
+        ['Pierre', 43, 'Proctologue'],
+        ['Jacques', 27, 'Ceinture noire de Yoga'],
+        ['Nathalie', 25, 'Casseuse de pierre']
+    ],
+    'Pros' => [
+        ['Jean-Pierre', 47, 'Assembleur de planches'],
+        ['Edgard', 67, 'Testeur de chemises']
+    ]
+];
+
+
+$clients = array(
+    'Particuliers' => array(
+        array('Pierre', 43, 'Proctologue'),
+        array('Jacques', 27, 'Ceinture noire de Yoga'),
+        array('Nathalie', 25, 'Casseuse de pierre')
+    ),
+    'Pros' => array(
+        array('Jean-Pierre', 47, 'Assembleur de planches'),
+        array('Edgard', 67, 'Testeur de chemises')
+    )
+);
+
+echo '<br>';
+
+// Ex2 #2 • Afficher seulement les clients ayant les plus de 36 ans.
+
+echo '<ul>';
+foreach ($clients as $key => $value) {
+    echo '<li>';
+
+    foreach ($value as $k) {
+
+        foreach ($k as $cle => $v) {
+
+            if ($k[1] > 36) {
+                echo "$v ";
+            }
+        }
+        echo '</li>';
+    }
+}
+echo '</ul>';
+echo '<br>';
+// Ex2 #3 Sans boucle, vérifier si le client « Jean-Pierre » existe et afficher une phrase selon s’il existe ou
+// non.
+
+if ($clients['Pros'][0][0] === 'Jean-Pierre') {
+    echo "Yes, Jean-Pierre exists!";
+}
+echo '<br>';
+
+// Ex2 #3 • Sans boucle, afficher dans une phrase, les informations de « Nathalie ».
+
+echo "{$clients['Particuliers'][2][0]} is {$clients['Particuliers'][2][1]} years old and she is \" {$clients['Particuliers'][2][2]} \" .";
+
+
+// Ex3 #1  Créer un tableau multidimensionnel associatif dont les clés sont des noms de personne et les
+//valeurs des tableaux contenant le prénom, l’âge et la ville de résidence.
+//Afficher le tableau avec le fonction PHP « print_r() ».
+
+$persons = [
+    'Por' => 'Anda'
+    'Minka' => 'Maija'
+    'Fab' => 'Jordan' 
+    'Robez'=>'Kay'
+]
