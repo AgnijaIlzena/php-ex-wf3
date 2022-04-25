c73eccabaec427d6255ea77e625c764b5fe05d4b
Agnija Ilzena <ilzena.agnija@gmail.com>
2022-04-25 10:22:55 +0200

    DB

---
+++ php/bdd/connexion.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Connexion à la base de données
+ */
+
+ // Localisation de la BDD
+ const DB_HOST = 'localhost';
+
+ // Nom d'utilisator
+ const DB_USER = 'root';
+
+ // mot de pass
+ const DB_PASS = '';
+
+ // Nom de la base de données
+ const DB_NAME = 'entrainement';
